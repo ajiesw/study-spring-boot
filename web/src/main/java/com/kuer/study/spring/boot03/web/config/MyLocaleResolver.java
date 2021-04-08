d73b358ddbe416af8bda15cb1135f4b3c8e85e3e
kuer <ajiesw.outlook.com>
2021-04-09 01:42:35 +0800

    spring国际化

---
+++ web/src/main/java/com/kuer/study/spring/boot03/web/config/MyLocaleResolver.java
@@ -0,0 +1,38 @@
+package com.kuer.study.spring.boot03.web.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.util.StringUtils;
+import org.springframework.web.servlet.LocaleResolver;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Locale;
+
+public class MyLocaleResolver implements LocaleResolver {
+    /**
+     * 解析请求
+     * @param httpServletRequest
+     * @return
+     */
+    @Override
+    public Locale resolveLocale(HttpServletRequest httpServletRequest) {
+
+        // 获取请求的语言参数
+        String language = httpServletRequest.getParameter("l");
+
+        Locale locale = Locale.getDefault();
+
+        if (StringUtils.hasText(language)){
+            // zh_CN
+            String[] split = language.split("_");
+            locale = new Locale(split[0], split[1]);
+        }
+
+        return locale;
+    }
+
+    @Override
+    public void setLocale(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Locale locale) {
+
+    }
+}
---
+++ web/src/main/java/com/kuer/study/spring/boot03/web/config/MyMvcConfig.java
@@ -0,0 +1,31 @@
+package com.kuer.study.spring.boot03.web.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.Locale;
+
+/**
+ * @author kuer
+ */
+@Configuration
+public class MyMvcConfig implements WebMvcConfigurer {
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("index");
+        registry.addViewController("/index.html").setViewName("index");
+    }
+
+    /**
+     * 自定义国际化
+     * @return
+     */
+    @Bean
+    public LocaleResolver localeResolver(){
+        return new MyLocaleResolver();
+    }
+}
